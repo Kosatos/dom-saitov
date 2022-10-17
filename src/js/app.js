570bc7fb186c40424e79adaeb35afd25e15b528d
Константин Музыка <kosatkinms@mail.ru>
2022-10-18 02:56:28 +0300

    header done

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,35 @@
 window.onload = () => {
+  const dropdownCaller = document.getElementById('header-dropdown-caller'),
+    searchCaller = document.getElementById('header-search-caller'),
+    headerDropdown = document.getElementById('header-dropdown'),
+    headerSearch = document.getElementById('header-search');
+
+  dropdownCaller.addEventListener('click', (evt) =>
+    callerClickHandler(evt, headerDropdown)
+  );
+  searchCaller.addEventListener('click', (evt) =>
+    callerClickHandler(evt, headerSearch)
+  );
+
+  function callerClickHandler(evt, el) {
+    evt.preventDefault();
+    if (!el.classList.contains('open') && !el.classList.contains('close')) {
+      el.classList.toggle('open');
+      return;
+    } else if (el.classList.contains('open')) {
+      el.classList.toggle('close');
+      setTimeout(() => el.classList.toggle('open'), 300);
+    } else if (el.classList.contains('close')) {
+      el.classList.toggle('close');
+      el.classList.toggle('open');
+    } else {
+      return;
+    }
+  }
+
   // burger
-  // let burgerMenu = document.querySelector('.burger-header');
-  // let menu = document.querySelector('.nav-header__list');
+  // let burgerMenu = document.getElementById('.burger-header');
+  // let menu = document.getElementById('.nav-header__list');
   // if (burgerMenu != null) {
   //   burgerMenu.addEventListener('click', function (e) {
   //     if (!burgerMenu.classList.contains('_active')) {
