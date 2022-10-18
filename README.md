cc74ed371e4ffc8f3122d268d5b917f73a6eceb3
Константин Музыка <kosatkinms@mail.ru>
2022-10-19 00:13:10 +0300

    project finished

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Дом Сайтов
 
 [Дом Сайтов](https://kosatos.github.io/dom-saitov/)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/q8j99b8xrrryo9f4?svg=true)](https://ci.appveyor.com/project/Kosatos/dom-saitov)
+
+- Выпадающий список✅
+- Бургер-меню✅
+- Адаптив✅
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,21 @@
 window.onload = () => {
+  // работа с вызовом dropdown  и поисковой стоки
+
   const dropdownCaller = document.getElementById('header-dropdown-caller'),
     searchCaller = document.getElementById('header-search-caller'),
     headerDropdown = document.getElementById('header-dropdown'),
-    headerSearch = document.getElementById('header-search');
+    headerSearch = document.getElementById('header-search'),
+    headerDropdownMini = document.getElementById('header-dropdown-mini'),
+    burgerMenu = document.getElementById('burger-menu'),
+    menu = document.getElementById('nav-menu');
 
-  dropdownCaller.addEventListener('click', (evt) =>
-    callerClickHandler(evt, headerDropdown)
-  );
+  dropdownCaller.addEventListener('click', (evt) => {
+    if (menu.classList.contains('active')) {
+      callerClickHandler(evt, headerDropdownMini);
+      return;
+    }
+    callerClickHandler(evt, headerDropdown);
+  });
   searchCaller.addEventListener('click', (evt) =>
     callerClickHandler(evt, headerSearch)
   );
@@ -27,19 +36,47 @@ window.onload = () => {
     }
   }
 
+  // переключение контанта в блоке "Мы всегда достигаем обещанного результата"
+
+  const qualities = [
+    `Quasi eaque, fugiat repellendus eveniet reprehenderit nesciunt error, placeat exercitationem soluta repudiandae, aperiam cumque alias quis deserunt.`,
+    `Даём юридические финансовые гарантии выполнения своих обязательств.
+    Более 60 высококвалифицированных специалистов в штате - мы обладаем ресурсами и
+    технологиями для решения любой задачи точно в срок.`,
+    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident nobis aliquam eius blanditiis ipsam nostrum omnis maxime, dolorem, neque deserunt laborum distinctio modi amet vero dolorum culpa quaerat quae est.`,
+    `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum neque molestias soluta ut tenetur minima voluptatibus nulla assumenda nobis nesciunt!`,
+    `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, deserunt?`,
+    `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nam earum asperiores modi dolor eum.`,
+  ];
+
+  const navigation = [
+    ...document.querySelectorAll('.slider-qualities__qualitie'),
+  ];
+
+  navigation.map((el, idx) => {
+    el.addEventListener('click', () => changeContent(el, idx));
+  });
+
+  function changeContent(el, idx) {
+    navigation.map((el) => el.classList.remove('active'));
+    el.classList.toggle('active');
+    document.querySelector('.slider-qualities__slide').textContent =
+      qualities[idx];
+  }
+
   // burger
-  // let burgerMenu = document.getElementById('.burger-header');
-  // let menu = document.getElementById('.nav-header__list');
-  // if (burgerMenu != null) {
-  //   burgerMenu.addEventListener('click', function (e) {
-  //     if (!burgerMenu.classList.contains('_active')) {
-  //       burgerMenu.classList.toggle('_active');
-  //       menu.classList.toggle('_active');
-  //       return;
-  //     } else {
-  //       burgerMenu.classList.toggle('_active');
-  //       menu.classList.toggle('_active');
-  //     }
-  //   });
-  // }
+
+  if (burgerMenu != null) {
+    burgerMenu.addEventListener('click', function (e) {
+      if (!burgerMenu.classList.contains('active')) {
+        burgerMenu.classList.toggle('active');
+        menu.style.display = 'flex';
+        setTimeout(() => menu.classList.toggle('active'), 100);
+        return;
+      } else {
+        burgerMenu.classList.toggle('active');
+        menu.classList.toggle('active');
+      }
+    });
+  }
 };
